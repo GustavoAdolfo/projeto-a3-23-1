29a706573e52c7563b977fcab5e2df15e0c6f8f2
Gustavo Adolfo <gustavo.adolfo.as@gmail.com>
2023-05-29 00:47:50 -0300

    chore: criando site web

---
+++ web/site/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Carro Detector de Obstátulos</h3>
+        <h5>USJT - Engenharia da Computação - 2023.1</h5>
+        <div className='equipe'>
+          <strong>Equipe: </strong>
+            Aquiles Colares de Oliveira - 820144933 &diams; Guilherme de Morais Cavalcante - 819154676 &diams; 
+            Gustavo Adolfo Alencar - 820271356 &diams; Henrique Manuel dos Santos Costa - 819120677 &diams; 
+            Rogério dos Santos Cruz - 822166361          
+        </div>
+      </header>
+
+      <main>
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
